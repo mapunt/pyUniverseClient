7fe60ab4ba61ce116f2db13144eb39e5678b8875
Marino Punturieri <mapunt@gmail.com>
2017-07-31 22:07:55 +0200

    Create config.py
    First configuration file

---
+++ config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8
+
+# Created on 2017.07.31
+#
+# Author: Marino Punturieri
+#
+#
+# This file is part of pyUniverseClient
+
+# LDAP Server and credentials info
+
+host = 127.0.0.1
+port = 1234
+user = test
+pwd = test
